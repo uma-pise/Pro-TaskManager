2be62e460c2831c581400ae101403658a885e647
uma_pise <59907020+uma-pise@users.noreply.github.com>
2024-10-31 22:37:56 +0530

    Update taskController.js

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -100,8 +100,8 @@ deleteTask: async (req, res) => {
 
 generateLink: async (taskId) => {
 
-  //const Url = 'https://promanage.vercel.app';
-  const Url = 'https://localhost:3000';
+  const Url = 'pro-task-manager-five.vercel.app';
+  //const Url = 'https://localhost:3000';
   try {
     const task = await Task.findById({_id:taskId});
 
